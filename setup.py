012fe0bb37d4b1e9d9705441ea761876be242bfb
Carlos Martinez <carlos@queo.pt>
2021-04-14 17:20:06 +0100

    better setup

--- setup.py
+++ setup.py
@@ -4,5 +4,9 @@ from setuptools import setup
 
 setup(
     name="orjson",
+    version="3.5.1",
     url="https://github.com/ijl/orjson",
+    description="orjson",
+    authors=["orjson"],
+    repository="https://github.com/ijl/orjson/",
 )
